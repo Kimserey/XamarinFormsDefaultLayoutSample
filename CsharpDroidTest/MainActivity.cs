007484430f281d8b11368dc2f8dd15d070c876a7
Kimserey Lam <kimserey.lam@gmail.com>
2016-09-05 16:10:34 +0100

    added csharp project

---
+++ CsharpDroidTest/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Forms;
+using Android.Content.PM;
+
+namespace CsharpDroidTest
+{
+	[Activity(Label = "CsharpDroidTest", Theme = "@style/MyTheme", MainLauncher = true, Icon = "@mipmap/icon", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			TabLayoutResource = Resource.Layout.Tabbar;
+			ToolbarResource = Resource.Layout.Toolbar;
+
+			base.OnCreate(bundle);
+			Xamarin.Forms.Forms.Init(this, bundle);
+			this.LoadApplication(new XfDefaultLayoutSample.App());
+		}
+	}
+}
+
+
